50584c8104a68316074df2a77f0c090403be8d1e
BINK <bink@email.ua>
2022-07-13 11:57:48 +0300

    human-readble-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "zero",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 20) {
+        return ones[number];
+    } else if (number < 100) {
+        return (
+            tens[Math.floor(number / 10)] +
+            (number % 10 === 0 ? "" : " " + ones[number % 10])
+        );
+    } else {
+        return (
+            ones[Math.floor(number / 100)] +
+            " hundred" +
+            (number % 100 === 0 ? "" : " " + toReadable([number % 100]))
+        );
+    }
+};
